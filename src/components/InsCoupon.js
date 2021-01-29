e47f565ebaba278a20d7b7e4a3d19529ff0db7ba
with_tjlee@naver.com <with_@naver.com>
2021-01-29 18:29:30 +0900

    api url 수정

--- src/components/InsCoupon.js
+++ src/components/InsCoupon.js
@@ -54,7 +54,7 @@ const InsCoupon = (props) => {
 
   const handleClick = (e) => {
     e.preventDefault();
-    const apiurl = "http://ec2-54-180-150-6.ap-northeast-2.compute.amazonaws.com:8080/api/coupon/create";
+    const apiurl = "http://localhost:8080/api/coupon/create";
     const object = email;
     axios
     .post(apiurl, object)
